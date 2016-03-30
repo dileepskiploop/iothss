86798770831760004da71b2519ab223c90d777dd
dsouzaedison <dsouzaedison11@gmail.com>
2016-03-30 09:51:34 +0530

    server img

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
-
+var path = require('path');
+var url = "http://localhost:3000/intruder";
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0",function(){
 	console.log('Server Running...At : ' + process.env.IP + ':' + process.env.port);
 });
 
+app.use(express.static(path.join(__dirname, 'img')));
+
 app.locals.count = "4";
-app.locals.data = [{"desc":"An intrusion was detected recently at your house.Photos have been attached with this card.Please review and take necessary actions immediately."},{"desc":"This works well with NodeJS."},{"desc":"Please be aware of your valuable belongings!!"},{"desc":"Testing XHR Resquests to Express."}];
+app.locals.data = [{"desc":"An intrusion was detected recently at your house.Photos have been attached with this card.Please review and take necessary actions immediately.","img": url + "1.jpg"},{"desc":"This works well with NodeJS.","img": url + "2.jpg"},{"desc":"Please be aware of your valuable belongings!!","img": url + "3.jpg"},{"desc":"Testing XHR Resquests to Express.","img": url + "4.png"}];
 
 app.get('/intrusion',function(req,res){
 	res.header('Access-Control-Allow-Origin','*');
@@ -18,4 +21,23 @@ app.get('/count',function(req,res){
 	res.send(app.locals.count);
 });
 
-//Testing push sync from github
+//Login
+
+var users = [{'user' : 'edison', 'pass' : 'pass'} , {'user' : 'admin', 'pass' : 'pass'}];
+
+app.get('/login/:user/:pass',function(req,res){
+	res.header('Access-Control-Allow-Origin','*');
+	console.log('User : ' + req.params.user);
+	console.log('Pass : ' + req.params.pass);
+	for(i=0;i<users.length;i++)
+	{
+		console.log('users.user : ' + users[i].user + '\nusers.pass : ' + users[i].pass);
+	if(users[i].user == req.params.user && users[i].pass == req.params.pass)
+	res.end("true");
+	}
+	res.end("false");
+});
+
+app.get('/img',function(req,res){
+	res.sendFile(__dirname + '/img/img.png');
+});
